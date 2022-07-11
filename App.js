091c52348d134df00b4187166fecc4224cbf1e00
Rakha112 <rakhawibowo1998@gmail.com>
2022-07-11 19:46:24 +0700

    Add redux

--- App.js
+++ App.js
@@ -7,28 +7,50 @@ import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import BottomTabNavigation from './src/components/BottomTabNavigation';
 import ToastComponent from './src/components/ToastComponent';
 import ProdukPage from './src/screen/ProdukPage';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
 const App = () => {
   const Stack = createStackNavigator();
+
+  const initailState = {
+    loading: true,
+  };
+
+  const rootReducer = (state = initailState, action) => {
+    switch (action.type) {
+      case 'LOADING':
+        return {
+          ...state,
+          loading: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const store = configureStore({reducer: rootReducer});
   return (
-    <SafeAreaProvider>
-      <StatusBar
-        backgroundColor="transparent"
-        translucent={true}
-        barStyle="dark-content"
-      />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerShown: false,
-            ...TransitionPresets.SlideFromRightIOS,
-          }}>
-          <Stack.Screen name="Home" component={BottomTabNavigation} />
-          <Stack.Screen name="Produk" component={ProdukPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <ToastComponent />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <StatusBar
+          backgroundColor="transparent"
+          translucent={true}
+          barStyle="dark-content"
+        />
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerShown: false,
+              ...TransitionPresets.SlideFromRightIOS,
+            }}>
+            <Stack.Screen name="Home" component={BottomTabNavigation} />
+            <Stack.Screen name="Produk" component={ProdukPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <ToastComponent />
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
